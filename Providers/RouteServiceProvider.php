f04ab2b92759312e66d7438a3fbecb37788ec645
ifui <ifui@foxmail.com>
2022-04-07 20:59:23 +0800

    chore: 修改Admin路由中间件

--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -62,7 +62,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-            ->middleware('web')
+            ->middleware('api')
             ->namespace($this->moduleNamespace)
             ->group(module_path('Pay', '/Routes/api.php'));
     }
