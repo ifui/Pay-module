258227abe01cd3c2b973729e6ac97363c766c536
ifui <ifui@foxmail.com>
2022-07-03 22:55:17 +0800

    chore: update doc

--- Http/Controllers/V1/AlipayController.php
+++ Http/Controllers/V1/AlipayController.php
@@ -3,9 +3,12 @@
 namespace Modules\Pay\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
-use Faker\Core\Number;
-use Illuminate\Support\Facades\Response;
+use Illuminate\Http\JsonResponse;
 use Modules\Pay\Entities\PayOrder;
+use Psr\Http\Message\ResponseInterface;
+use Yansongda\Pay\Exception\ContainerException;
+use Yansongda\Pay\Exception\InvalidParamsException;
+use Yansongda\Pay\Exception\ServiceNotFoundException;
 use Yansongda\Pay\Pay;
 
 class AlipayController extends Controller
@@ -14,7 +17,10 @@ class AlipayController extends Controller
      * 支付宝查询订单是否支付完成接口
      *
      * @param string $id
-     * @return Response
+     * @return JsonResponse
+     * @throws ContainerException
+     * @throws InvalidParamsException
+     * @throws ServiceNotFoundException
      */
     public function check($id)
     {
@@ -39,8 +45,10 @@ class AlipayController extends Controller
     /**
      * 接收阿里回调方法
      * 确认交易是否成功
-     * 
-     * @return Response
+     *
+     * @return JsonResponse|ResponseInterface
+     * @throws ContainerException
+     * @throws InvalidParamsException
      */
     public function callback()
     {
@@ -53,7 +61,7 @@ class AlipayController extends Controller
         }
 
         $app_id = config('pay.alipay.default.app_id');
-        $price = (string) $model->price;
+        $price = (string)$model->price;
 
         // 对请求结果进行校验，不正确直接返回
         if ($result->total_amount !== $price or $result->app_id !== $app_id) {
